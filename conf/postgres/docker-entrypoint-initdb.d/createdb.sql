77eacb254980266ad2310f927df5d1200279e93d
Chen Xin <chenxin.az@outlook.com>
2020-06-18 04:07:21 +0000

    create simple branch

--- conf/postgres/docker-entrypoint-initdb.d/createdb.sql
+++
@@ -1,9 +0,0 @@
-create user gogs ENCRYPTED PASSWORD 'f246-851b-ea09-1d14-d424';
-create database gogs owner gogs;
-
-create user gitea ENCRYPTED PASSWORD '7a06-70ee-b6d6-e39b-2d69';
-create database gitea owner gitea;
-
-create user drone ENCRYPTED PASSWORD 'bc1c-31c7-ef16-3d48-79f4';
-create database drone owner drone;
-
