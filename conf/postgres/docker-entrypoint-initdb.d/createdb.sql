894d0c164dd7f6c269913d36d04f5280fea6af02
Xin <chenxin.az@outlook.com>
2020-11-11 08:52:45 +0000

    simple config for arm server

--- conf/postgres/docker-entrypoint-initdb.d/createdb.sql
+++ conf/postgres/docker-entrypoint-initdb.d/createdb.sql
@@ -1,9 +1,5 @@
-create user gogs ENCRYPTED PASSWORD 'f246-851b-ea09-1d14-d424';
-create database gogs owner gogs;
-
-create user gitea ENCRYPTED PASSWORD '7a06-70ee-b6d6-e39b-2d69';
+create user gitea ENCRYPTED PASSWORD 's4IpyIsZtyFmbdjDcrkdhPzPfYQZosD4qPJgjM79ZAI=';
 create database gitea owner gitea;
 
-create user drone ENCRYPTED PASSWORD 'bc1c-31c7-ef16-3d48-79f4';
-create database drone owner drone;
-
+create user xin ENCRYPTED PASSWORD 'zNLgm/aS/Bp9M5FTUaMFx8oJjJQB5+K9mteKsLF0WYQ=';
+create database dev owner xin;
